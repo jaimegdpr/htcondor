b11971b95dfe33ae57a8ef0c75afe0eae4bac6a7
AntonioDelgado <antonio.delgado.peris@cern.ch>
2017-10-30 11:27:07 +0100

    Fix minor bug

--- manifests/scripts.pp
+++ manifests/scripts.pp
@@ -2,13 +2,6 @@ class htcondor::scripts {
     
     $script_files = $htcondor::script_files_list
 
-    file {'/root/scripts':
-        ensure => directory,
-        owner => 'root',
-        group => 'root',
-        mode => '0755',
-    }
-
     $script_files.each |String $script_file| {
         file {"/root/scripts/${script_file}":
             ensure => present,
